3fae7f569f93333ca7b3085e619f27f7efda7253
Gabriel Correia <140673462+GabrielGabuu@users.noreply.github.com>
2024-09-03 21:57:02 -0300

    create db: table games, books and mangas

---
+++ database/migrations/2024_09_04_003029_mangas.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mangas', function(Blueprint $table){
+            $table->id();
+            $table->string('title');
+            $table->text('image')->nullable();
+            $table->string('genre');
+            $table->string('classification',2);
+            $table->text('synopsis');
+            $table->integer('chapter');
+            $table->integer('volume');
+            $table->string('author');
+            $table->date('publication_date')->nullable();
+            $table->string('status');
+            $table->integer('rating')->nullable();
+            $table->boolean('favorite')->nullable();
+            $table->text('comment')->nullable();
+            $table->string('developed_by');
+            $table->string('plataform');
+            $table->foreignUuid('user_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mangas');
+    }
+};
