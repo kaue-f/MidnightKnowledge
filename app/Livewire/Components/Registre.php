ddac99db9e967be219a89b178adc0c8da846e13d
Gabriel Correia <140673462+GabrielGabuu@users.noreply.github.com>
2024-10-25 21:37:21 -0300

    :created: layout de registro

--- app/Livewire/Components/Registre.php
+++ app/Livewire/Components/Registre.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire\Components;
 
+use App\DTO\UserDTO;
 use Livewire\Component;
 
 class Registre extends Component
 {
+    public array $userDTO;
     public function render()
     {
         return view('livewire.components.registre');
+    }
+
+    public function mount()
+    {
+        $this->userDTO = (array) new UserDTO;
+    }
+    
+    public function register()
+    {
+        dd($this->userDTO['name']);
     }
 }
