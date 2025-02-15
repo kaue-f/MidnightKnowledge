fae607fc352525825f03e1f8e30acbf07d9f85ba
MatheusOliveira3633 <matheusoliveira3633@outlook.com>
2025-02-14 21:35:59 -0300

    Capslock function was removed

--- resources/views/livewire/components/login.blade.php
+++ resources/views/livewire/components/login.blade.php
@@ -1,12 +1,9 @@
 <x-form wire:submit="login" no-separator class="w-full">
     <div class="space-y-4">
         <x-input label="Nickname ou e-mail" wire:model='loginForm.user' />
-        <div class="tooltip-secondary" id="capslockAlert" data-tip="CapsLock Ativo">
-            <x-password id="passwordInput" maxlength="25" label="Senha" wire:model='loginForm.password' right />
-        </div>
+        <x-password  maxlength="25" label="Senha" wire:model='loginForm.password' right />
         <div class="flex justify-between text-xs py-2 text-base-content/75">
-            <x-checkbox class="border-0 checkbox-sm rounded-md bg-accent" label="Lembre-me"
-                wire:model="loginForm.remember" />
+            <x-checkbox class="border-0 checkbox-sm rounded-md bg-accent" label="Lembre-me" wire:model="loginForm.remember" />
             <a href="#" class=" hover:underline hover:text-purple-400">Esqueceu senha</a>
         </div>
         <x-slot:actions>
@@ -14,7 +11,3 @@
         </x-slot:actions>
     </div>
 </x-form>
-
-@push('scripts')
-    <script src="js/capslock-alert.js"></script>
-@endpush
