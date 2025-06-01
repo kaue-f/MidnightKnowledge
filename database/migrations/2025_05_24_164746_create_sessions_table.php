8458d87413097751b85c09ebd35d150d4cc8b894
Kaue Francisco <kaue_francisco70@hotmail.com>
2025-06-01 14:48:57 -0300

    :bug: fix: fix user session issue.

--- database/migrations/2025_05_24_164746_create_sessions_table.php
+++ database/migrations/2025_05_24_164746_create_sessions_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->foreignUuid('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
