c4f100a938d8f3e79e63e5c635afcc97ac2a1372
GabrielGabuuu <gabru.com.br@outlook.com>
2025-09-10 22:06:26 -0300

    :recycle:  Refactor:  images from: signup and login changed, plus bg login added

--- resources/views/livewire/pages/welcome.blade.php
+++ resources/views/livewire/pages/welcome.blade.php
@@ -1,6 +1,6 @@
-<div class="flex flex-col w-full max-w-md mx-auto gap-6 justify-center items-center px-4">
+<div class="flex flex-col w-full max-w-md mx-auto gap-6 justify-center items-center px-4 ">
     <a href="/" wire:navigate class="mt-6 sm:mt-0">
-        <img src="{{ asset('images/layouts/Logo2.png') }}" class="h-28 sm:h-40">
+        <img src="{{ asset('images/midnight/midnight-horizontal.png') }}" class="h-28 sm:h-40">
     </a>
 
     <div class="w-full bg-base-200 p-6 sm:p-8 rounded-xl"
