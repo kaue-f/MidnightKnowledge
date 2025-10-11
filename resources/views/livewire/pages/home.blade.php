080e27ee7aed555e049bde40a68f81f13dc54e22
GabrielGabuu <gabru.com.br@outlook.com>
2025-10-10 23:06:38 -0300

    home warning

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -1,3 +1,8 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="w-full h-full flex justify-center items-center flex-col">
+    <div class="w-50">
+        <img src="{{asset('images/midnight/coruja-work-in-progress.png')}}" alt="">
+    </div>
+    <div class="text-3xl font-bold">
+        <h1>Não tem nada para ver aqui!</h1>
+    </div>
 </div>
